dfc9ab301cf3fab085d80519ca91acfe6cc66b5c
Facundo Jaime <164009725+HolyTostadora@users.noreply.github.com>
2024-05-28 09:00:21 -0300

    Password input and check

---
+++ Assets/CompPass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CompPass : MonoBehaviour
+{
+    public string Password;
+    public InputField input_Password;
+
+    public void CheckPass()
+    {
+        if (Password == input_Password.text)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+}
